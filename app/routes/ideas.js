d95bfa93f54b5222cc8cc394e27d9bd31ebbff32
Paul Grever <paulgrever@gmail.com>
2015-06-03 10:52:51 -0600

    added model call to routes file

--- app/routes/ideas.js
+++ app/routes/ideas.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // getting a model for idea
+  model: function () {
+    return this.store.find('idea');
+    // this is eqivelent to Idea.all in rails
+  }
 });
