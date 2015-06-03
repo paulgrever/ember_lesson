9bd9ef889572cf96f1b72b5639453767e470a9d7
Paul Grever <paulgrever@gmail.com>
2015-06-03 10:45:03 -0600

    added export Idea in idea.js and which allows it to be used elsewhere

--- app/models/idea.js
+++ app/models/idea.js
@@ -1,8 +1,11 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+let Idea =  DS.Model.extend({
   // seting up what acts like DB
   title: DS.attr('string'),
   body: DS.attr('string')
   
 });
+
+
+export default Idea;
