8207e74011774bd9fbe9adb5d246f478e6c6e983
Paul Grever <paulgrever@gmail.com>
2015-06-03 11:49:52 -0600

    added edit function and removed css that was hiding it

--- app/components/awesome-idea.js
+++ app/components/awesome-idea.js
@@ -5,6 +5,16 @@ export default Ember.Component.extend({
   editing: false, 
 
   actions: {
+    edit: function() {
+      let editing = this.get('editing');
+      if (editing) {
+        this.get('idea').save().then(() => {
+          this.set('editing', false);
+        })
+      } else {
+        this.set('editing', true);
+      }
+    },
     delete: function() {
       this.get('idea').destroyRecord();
     }
