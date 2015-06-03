007a56ac794de11b772ce03f3343f85b983d6dfc
Paul Grever <paulgrever@gmail.com>
2015-06-03 11:20:40 -0600

    added contoller routes action

--- app/controllers/ideas.js
+++ app/controllers/ideas.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // these are your controller routes
+  title: null, 
+  body: null, 
+  actions: {
+    addNewIdea: function() {
+      console.log("hi")
+    }
+  }
 });
